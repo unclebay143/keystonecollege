9591a6966e5515709e031636280f2d02550a0436
unclebay143 <58919619+unclebay143@users.noreply.github.com>
2020-01-31 10:40:45 -0800

    add login to accounts

--- keystoneaccounts/urls.py
+++ keystoneaccounts/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import Dashboard,Login
 
-
-
 urlpatterns = [
     path('dashboard/', Dashboard, name="dashboard"),
     path('login/', Login, name="login"),
-
-
 ]
